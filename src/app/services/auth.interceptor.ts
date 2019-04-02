fc4f21de28a03dd554c07b4d64e4ae28aacf0ba1
Iceemrr <Iceemrr@aim.com>
2019-04-02 14:21:51 -0400

    Add header image

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,16 +1,17 @@
+import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private auth: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       withCredentials: true,
       setHeaders: {
-        Authorization: 'Pizza'
+        Authorization: 'Bearer ${this.auth.getToken()}'
       }
   });
   return next.handle(request);
