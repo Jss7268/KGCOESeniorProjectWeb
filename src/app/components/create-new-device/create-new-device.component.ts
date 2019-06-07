0751b502970ac1c01a5005f4d6ef6f2e2604a358
MukeytheMuse <kimsookoo121@gmail.com>
2019-06-07 15:18:52 -0400

    Beginning set up on component.ts for creating a new device in the system

--- src/app/components/create-new-device/create-new-device.component.ts
+++ src/app/components/create-new-device/create-new-device.component.ts
@@ -1,4 +1,10 @@
+import { ConfirmationDialogComponent } from './../confirmation-dialog/confirmation-dialog.component';
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-create-new-device',
@@ -6,8 +12,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-new-device.component.css']
 })
 export class CreateNewDeviceComponent implements OnInit {
+  ngUnsubscribe = new Subject();
+  createDeviceForm: FormGroup;
+  submitted: boolean;
 
-  constructor() { }
+  constructor(private createDeviceService: UserService, private formBuilder: FormBuilder,
+    private route: ActivatedRoute, private router: Router, public dialog: MatDialog) { }
 
   ngOnInit() {
   }
