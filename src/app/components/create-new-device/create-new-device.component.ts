8b5bb68ee2b167d0ff944bbbcad150781402d368
MukeytheMuse <kimsookoo121@gmail.com>
2019-06-07 01:41:20 -0400

    Created the create-new-device section under components

---
+++ src/app/components/create-new-device/create-new-device.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-device',
+  templateUrl: './create-new-device.component.html',
+  styleUrls: ['./create-new-device.component.css']
+})
+export class CreateNewDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
