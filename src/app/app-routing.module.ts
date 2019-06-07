d6ab3a40ff14b06ffd823cbb2909d0a706997806
MukeytheMuse <kimsookoo121@gmail.com>
2019-06-07 15:16:19 -0400

    HTML page for adding a new device set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,20 @@ import { AuthGuard } from 'src/app/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { CreateExperimentComponent } from './components/create-experiment/create-experiment.component';
 import { CreateDeviceExperimentComponent } from './components/create-device-experiment/create-device-experiment.component';
+import { CreateNewDeviceComponent } from './components/create-new-device/create-new-device.component';
 
 const routes: any = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'unauthorized', component: HomeComponent, pathMatch: 'full' },
-  { path: '', component: HomeComponent, pathMatch: 'full', authLevel: 0, canActivate: [AuthGuard] },
-  { path: 'home', component: HomeComponent, pathMatch: 'full', authLevel: 0, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent, pathMatch: 'full', authLevel: 0 },
+  { path: 'home', component: HomeComponent, pathMatch: 'full', authLevel: 0 },
   { path: ListExperimentsComponent.path, component: ListExperimentsComponent, pathMatch: 'full', authLevel: 0, canActivate: [AuthGuard] },
   { path: 'experiments/create', component: CreateExperimentComponent, authLevel: 2, canActivate: [AuthGuard] },
   { path: 'device-outputs/create', component: CreateDeviceOutputComponent, authLevel: 1, canActivate: [AuthGuard] },
-  { path: 'output-types/create', component: CreateOutputTypeComponent, authLevel: 1, canActivate: [AuthGuard] },
+  { path: 'output-types/create', component: CreateOutputTypeComponent, authLevel: 1},
   { path: 'devices-experiments/create', component: CreateDeviceExperimentComponent, authLevel: 2, canActivate: [AuthGuard] },
+  { path: 'devices/create', component: CreateNewDeviceComponent, authLevel: 2},
 
 ];
 
