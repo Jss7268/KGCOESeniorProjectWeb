401daff36aed6e91a5d999a9f689a0d1d26910aa
Peace Omiponle <pto7724@rit.edu>
2019-08-06 16:12:07 -0400

    Welcome page, very barebones and minimal, but can be built upon (#35)
    * Welcome page, very barebones and minimal, but can be buildt upon
    
    * Code cleanup
    
    * Fixed removal of class from header
    
    * Per comments
    
    * Welcome page changes
    
    * Complete Welcome page
    
    * Cleanup

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,49 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from './../../services/user.service';
+import { Subscription } from 'rxjs';
+import { AppPaths } from 'src/app/app.paths';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   info: any;
+  $user: Subscription;
+  userId = '';
+  name = '';
+  accessLevel= '';
 
-  constructor(private auth: AuthService) { }
+
+  constructor(private auth: AuthService, private router: Router, public userService: UserService, private route: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.$user = this.userService.getCurrentUser().subscribe(
+      (data: any) => {
+        this.userId = data.id;
+        this.name = data.name;
+        this.accessLevel = data.access_level;
+      },
+      (error: any) => console.log(error)
+    );
+  }
+
+  settings() {
+    this.router.navigate([AppPaths.SETTINGS_PATH]);
+  }
+
+  getRequestAccessPath() {
+    return `/${this.route.parent.snapshot.url.join('/')}/${AppPaths.REQUEST_ACCESS_PATH}`;
   }
 
   test() {
     this.auth.getUsers().subscribe((data: any) => {
-      console.log(data);
       this.info = data;
     })
   }
